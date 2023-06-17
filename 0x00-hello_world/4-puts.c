bc2ea3e0c2365fc5cce91d8ea864d5ebba8847c1
Mindset-Dmax <emmanuelkipchirchir16@gmail.com>
2023-06-17 12:35:45 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-/*
- * main -Entry point
+
+/**
+ * main - Entry point
  *
- * Puts
+ * Return: Always 0
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
