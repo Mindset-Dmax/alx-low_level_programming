64e1f6321e516d8ff5862016eb0c47adeae13e75
Mindset-Dmax <emmanuelkipchirchir16@gmail.com>
2023-06-18 01:14:37 -0700

    prints the size of various types on the computer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 
 /**
- * main -Entry point
- *Description: size of various types on the computer
- * return: Always 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	char a;
-	int b;
-	long c;
-	long long int d;
-	float e;
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
 
-	printf("Size of a char: %d byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of an int: %d byte(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a long int: %d byte(s)\n", (unsigned long)sizeof(c);
-	printf("Size of a long long int: %d byte(s)\n", (unsigned long)sizeof(d));
-	printf("Size of a float: %d byte(s)\n", (unsigned long)sizeof(e));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
