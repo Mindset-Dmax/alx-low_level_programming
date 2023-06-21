29a0949ee331e5700aeeee8afe63c61294bfdad5
Mindset-Dmax <emmanuelkipchirchir16@gmail.com>
2023-06-21 12:36:42 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *  print_alphabet -Entry point
+ *  Return: Always 0
+ */
+void print_alphabet(void)
+	for (letter = 'a' ; letter <= 'z' ; letter++)
+{
+		_putchar("%c", &letter);
+}
