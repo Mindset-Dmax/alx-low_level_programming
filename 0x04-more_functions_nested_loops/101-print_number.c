d03152398ad247f9127f36f28005ef0b6dfb9ae2
Mindset-Dmax <emmanuelkipchirchir16@gmail.com>
2023-06-24 08:14:26 -0700

    prints an integer number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,7 +7,7 @@ int _putchar(char c);
 /**
  * print_number - like a hello world
  *
- * @n: params an integer
+ * params an integer
  *
  * No return
  */
@@ -21,7 +21,7 @@ void print_number(int n)
 	if  (n < 0)
 	{
 		n1 = -n;
-		putchar('\n');
+		_putchar('-');
 	}
 
 	else
@@ -34,5 +34,5 @@ void print_number(int n)
 		print_number(n1 / 10);
 	}
 
-	putchar((n1 % 10) + '0');
+	_putchar((n1 % 10) + '0');
 }
