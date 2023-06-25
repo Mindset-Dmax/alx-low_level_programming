e4f3462874f770eb420d0953d8725a69060ab429
Mindset-Dmax <emmanuelkipchirchir16@gmail.com>
2023-06-25 03:10:25 -0700

    adding the comments

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,18 +7,18 @@
 
 int main(void)
 {
-        int i;
-
-        printf("Infinite loop incoming :(\n"); // infinite loop incoming
-
-        i = 0;
+	int i;
+	
+	printf("Infinite loop incoming :(\n");
+	
+	i = 0;
 	/**
-	 * while (i < 10)
+	 * * while (i < 10)
 	 * {
 	 * putchar(i);
 	 * }
-	 */ 
-	printf("Infinite loop avoided! \\o/\n"); // infinite loop avoided
+	 */
+	printf("Infinite loop avoided! \\o/\n");
 
         return (0);
 }
